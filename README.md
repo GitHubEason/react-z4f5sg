edad382facc731bbaa53f0bbee5731cb65cc7b31
GitHubEason <defend.sen@gmail.com>
2021-12-08 16:38:52 +1100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-z4f5sg
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-z4f5sg)
---
+++ src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+import { Fragment } from 'react';
+import {Switch, Route} from "react-router-dom";
+import Results from "./results"
+import Income from "./income"
+import { TaxContext } from './TaxContext';
+import { useState } from 'react';
+
+const App = () => {
+  const [income,setIncome] = useState(null)
+  const [country, setCountry] = useState('Australia')
+  const [year, setYear] = useState('2021') 
+
+  return (
+    <Fragment>
+    <Switch>
+      <TaxContext.Provider value = {{value: [income, setIncome],value2: [country, setCountry],value3:[year, setYear]}}>
+      <Route exact path="/" component = {Income}></Route>
+      <Route exact path="/results" component = {Results}></Route>
+      </TaxContext.Provider>
+    </Switch>
+    </Fragment>
+  );
+}
+
+export default App;
